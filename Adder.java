b25fe07414dcfc5fabab656be1aed93396e75506
kongklaii <60050136@kmitl.ac.th>
2018-09-10 18:54:25 +0700

    add : adder.java

---
+++ Adder.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Adder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        int x = sc.nextInt();
+        int summ=x+y;
+        System.out.println(summ);
+    }
+}
